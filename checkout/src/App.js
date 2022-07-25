45c723fd84ae9b93eaf232d9d58fd668bb0471e1
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-07-25 14:36:09 +0000

    First Commit

---
+++ checkout/src/App.js
@@ -0,0 +1,12 @@
+import Checkout from './Components/Checkout';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout></Checkout>
+    </div>
+  );
+}
+
+export default App;
