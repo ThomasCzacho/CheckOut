95a6b41af2792004b8443d588cf52333646b5234
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-07-25 16:23:43 +0000

    Second Commit

--- checkout/src/Components/Review.js
+++ checkout/src/Components/Review.js
@@ -6,31 +6,38 @@ import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
 import { Avatar } from '@mui/material';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
+
+
+
 const products = [
   {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '$9.99',
+    name: 'Lions Knight Castle',
+    desc: 'Lions Knight Castle',
+    price: '$399.99',
+    src: '//www.lego.com/cdn/cs/set/assets/blt0254ea3dce736ea0/10305.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1.5',
 
     
   },
   {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '$3.45',
+    name: 'adidas Original Superstar',
+    desc: 'adidas Original Superstar',
+    price: '$89.99',
+    src: 'https://www.lego.com/cdn/cs/set/assets/blt87b29cdc866d4a4c/10282.jpg?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1.5'
   },
   {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '$6.51',
+    name: 'Atari® 2600',
+    desc: 'Atari® 2600',
+    price: '$239.99',
+    src: 'https://www.lego.com/cdn/cs/set/assets/blt10c72e2ab2785ea4/10306.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1.5'
   },
   {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '$14.11',
+    name: 'Chevrolet Camaro Z28',
+    desc: 'Chevrolet Camaro Z28',
+    price: '$169.99',
+    src: 'https://www.lego.com/cdn/cs/set/assets/blt729f7a96490ebf6f/10304.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1.5'
     
   },
-  { name: 'Shipping', desc: '', price: 'Free',},
+  { name: 'Shipping', desc: '', price: 'Free', src: 'https://www.colorhexa.com/ffffff.png',},
 ];
 
 const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
@@ -47,12 +54,14 @@ export default function Review() {
       <Typography variant="h6" gutterBottom>
         Order summary
       </Typography>
-      <List disablePadding>
+      <List padding='30px'>
         {products.map((product) => (
           <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
             <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
-            
+            <Typography variant="body2" sx={{ py: 1, px: 2 }}>{product.price}</Typography>
+            <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src={product.src} variant='square' sx={{ width: 56, height: 56 }} />
+        </ListItemAvatar>
             
           </ListItem>
         ))}
@@ -60,7 +69,7 @@ export default function Review() {
         <ListItem sx={{ py: 1, px: 0 }}>
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $34.06
+            $899.96
           </Typography>
         </ListItem>
       </List>
